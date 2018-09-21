40373fceb512b66d5a7e72d864010514966262da
UFo_x <tero.jaakkola1@gmail.com>
2018-09-21 12:45:27 +0300

    Add index story.

---
+++ .storybook/indexStory.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+
+storiesOf("Friba-components", module).add("getting started", () => (
+  <h1> Friba-components Storybook</h1>
+));
